a4512b51e149fd8986339d762c58c4f379b30034
dorakadri <dora.kadri@esprit.tn>
2023-03-08 21:55:30 +0100

    starting landing page

--- client/src/components/pages/UserInterface/landingcomponent/Navbar.jsx
+++ client/src/components/pages/UserInterface/landingcomponent/Navbar.jsx
@@ -1,34 +1,76 @@
-import { ArrowDropDownOutlined, DarkModeOutlined, Search, SettingsOutlined } from '@mui/icons-material'
-import { AppBar, Box, Button, CssBaseline, IconButton, InputBase, Link, Toolbar, Typography } from '@mui/material'
-import { borderRadius } from '@mui/system'
-import React from 'react'
-import FlexBetween from '../../../common/FlexBetween'
+import {
+  ArrowDropDownOutlined,
+  DarkModeOutlined,
+  Search,
+  SettingsOutlined,
+} from "@mui/icons-material";
+import {
+  AppBar,
+  Box,
+  Button,
+  CssBaseline,
+  IconButton,
+  InputBase,
+  Link,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import { borderRadius } from "@mui/system";
+import React from "react";
+import FlexBetween from "../../../common/FlexBetween";
 
 export default function Navbar() {
   return (
-    <Box sx={{ display: 'flex' }}>
+    <Box sx={{ display: "flex" }}>
       <CssBaseline />
-      <AppBar component="nav"   sx={{ position: "static",background:"none",boxShadow:"none" ,p:"1rem" }}>
+      <AppBar
+        component="nav"
+        sx={{
+          position: "static",
+          background: "none",
+          boxShadow: "none",
+          p: "1rem",
+        }}
+      >
         <Toolbar>
-         
           <Typography
             variant="h6"
             component="div"
-            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+            sx={{ flexGrow: 1, display: { xs: "none", sm: "block" } }}
           >
             Givly
           </Typography>
-          <Box sx={{ display: { xs: 'none', sm: 'flex' }  ,gap:"5rem" }}>
-          <Button  sx={{ color: 'black',px:"1rem",py:"0.5rem" }}>ABOUT US</Button>
-          <Button sx={{ color: 'black' ,px:"1rem" ,py:"0.5rem"}}> OUR PARTNER </Button>
-          <Button sx={{ color: 'black',px:"1rem" ,py:"0.5rem"}}> CONTACT US</Button>
-          <Button sx={{ color: 'white',px:"1rem",py:"0.5rem" , borderRadius:'30px' ,backgroundColor:"#06A696",  "&:hover": {
-                  color: 'black',
+          <Box sx={{ display: { xs: "none", sm: "flex" }, gap: "5rem" }}>
+            <Button sx={{ color: "black", px: "1rem", py: "0.5rem" }}>
+              ABOUT US
+            </Button>
+            <Button sx={{ color: "black", px: "1rem", py: "0.5rem" }}>
+              {" "}
+              OUR PARTNER{" "}
+            </Button>
+            <Button sx={{ color: "black", px: "1rem", py: "0.5rem" }}>
+              {" "}
+              CONTACT US
+            </Button>
+            <Button
+              sx={{
+                color: "white",
+                px: "1rem",
+                py: "0.5rem",
+                borderRadius: "30px",
+                backgroundColor: "#06A696",
+                "&:hover": {
+                  color: "black",
+                  backgroundColor: "#38BC73",
                 },
-        }}> Register</Button>
+              }}
+            >
+              {" "}
+              Register
+            </Button>
           </Box>
         </Toolbar>
       </AppBar>
-      </Box>
-  )
+    </Box>
+  );
 }
