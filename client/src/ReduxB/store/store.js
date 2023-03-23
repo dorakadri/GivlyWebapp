b677814e0401235d99361c74b43c7e50968419b6
unknown <eya_khechine@hotmail.com>
2023-03-23 23:26:53 +0100

    front redux forum

--- client/src/ReduxB/store/store.js
+++ client/src/ReduxB/store/store.js
@@ -2,12 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import usersReducer from "../slices/users/usersSlices";
 import globalReducer from "../../components/pages/Dashboard/state/index";
 import accountVerification from "../slices/accountVerification/accVerificationSlices";
-
+import postForum from "../slices/postsForum/postForumSlices";
+import comment from "../slices/comments/commentSlices";
 const store = configureStore({
   reducer: {
     users: usersReducer,
     dashboard: globalReducer,
     accountVerification,
+    postForum,
+    comment,
   },
 });
 
