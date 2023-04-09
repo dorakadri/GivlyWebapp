dba72aaaadd0f05ce4749f98f93aaff695460683
dorakadri <dora.kadri@esprit.tn>
2023-04-09 18:34:26 +0100

    fixed error in the nav bar

--- client/src/components/pages/SimpleUserProfile/Finalnavbar.js
+++ client/src/components/pages/SimpleUserProfile/Finalnavbar.js
@@ -127,14 +127,49 @@ setcount(count);
               anchorEl={anchorEl}
               open={open}
               onClose={handleClose}
-              anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+              onClick={handleClose}
+              PaperProps={{
+                elevation: 0,
+                sx: {
+                  overflow: "visible",
+                  filter: "drop-shadow(0px 2px 8px rgba(0,0,0,0.32))",
+                  mt: 1.5,
+                  "& .MuiAvatar-root": {
+                    width: 32,
+                    height: 32,
+                    ml: -0.5,
+                    mr: 1,
+                  },
+                  "&:before": {
+                    content: '""',
+                    display: "block",
+                    position: "absolute",
+                    top: 0,
+                    right: 14,
+                    width: 10,
+                    height: 10,
+                    bgcolor: "background.paper",
+                    transform: "translateY(-50%) rotate(45deg)",
+                    zIndex: 0,
+                  },
+                },
+              }}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
             >
               <MenuItem onClick={() => navigate(`./profile`)}>
                 {" "}
-                <Typography textAlign="center">Profile</Typography>
+               Profile
               </MenuItem>
               <MenuItem onClick={handelLogout}>
-                <Typography textAlign="center">Logout</Typography>
+              Logout
               </MenuItem>
             </Menu>
           </Box>
