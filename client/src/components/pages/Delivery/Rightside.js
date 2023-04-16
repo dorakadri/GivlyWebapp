2de13c67fee18842e1676fcf7263d0420dd99477
dorakadri <dora.kadri@esprit.tn>
2023-04-16 17:37:22 +0100

    detaildelivery

--- client/src/components/pages/Delivery/Rightside.js
+++ client/src/components/pages/Delivery/Rightside.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import * as React from 'react';
+import dayjs from 'dayjs';
+import Grid from '@mui/material/Grid';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
+import { TimeClock } from '@mui/x-date-pickers/TimeClock';
+import { Typography } from '@mui/material';
 
-export default function Rightside() {
+export default function Rightside(props) {
+  const selectedDate = dayjs(props.date);
+
   return (
-    <div>Rightside</div>
-  )
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Grid
+        container
+        columns={{ xs: 1, lg: 2 }}
+        spacing={4}
+        alignItems="center"
+        justifyContent="center"
+      >
+      
+        <Grid item>
+        
+            <Typography variant="caption" sx={{pl:"1rem"}} >     Delivery date  : </Typography>
+          <DateCalendar value={selectedDate} disableFuture />
+        </Grid>
+      </Grid>
+    </LocalizationProvider>
+  );
 }
