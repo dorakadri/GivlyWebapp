aabcf35a72b9e843c55101a8d33f2ef99d042043
unknown <eya_khechine@hotmail.com>
2023-04-19 08:14:07 +0100

    adding filter organization by theme

--- server/routes/Asso/AssoRoutes.js
+++ server/routes/Asso/AssoRoutes.js
@@ -1,30 +1,29 @@
 const express = require("express");
-const axios = require('axios')
-const cheerio = require('cheerio')
+const axios = require("axios");
+const cheerio = require("cheerio");
 
-const url = 'https://jamaity.org/associations'
+const url =
+  "https://jamaity.org/associations/?region=tunisie&theme=agriculture";
 const AssoRoutes = express.Router();
 
-AssoRoutes.get('/', function (req, res) {
-    res.json('This is my webscraper')
-  })
+AssoRoutes.get("/", function (req, res) {
+  res.json("This is my webscraper");
+});
 
+const articles = [];
 
-  const articles = []
+AssoRoutes.get("/results", (req, res) => {
+  const filteredData = articles.filter(
+    (obj) => obj.title.trim() !== "" && obj.imgSrc.trim() !== ""
+  );
 
-  AssoRoutes.get('/results', (req, res) => {
-    const filteredData = articles.filter(obj => obj.title.trim() !== '' &&  obj.imgSrc.trim() !== '');
+  res.json(filteredData);
+});
+axios(url)
+  .then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
 
-    res.json(filteredData)
-  
-  })
-  axios(url)
-  .then(response => {
-    const html = response.data
-    const $ = cheerio.load(html)
-  
-  
-  
     // $('.h4', html).each(function () { //<-- cannot be a function expression
     //     const title = $(this).text()
     //     const url = $(this).find('a').attr('href')
@@ -33,176 +32,225 @@ AssoRoutes.get('/', function (req, res) {
     //         url,
     //     })
     // })
-    $('div.panel').each(function () {
-        const href = $(this).find('a.text-left').attr('href');
-        const title = $(this).find('a.text-left').text();
-        const imgSrc = $(this).find('img').attr('src');
-        articles.push({
-            href, title, imgSrc
-        })
+    $("div.panel").each(function () {
+      const href = $(this).find("a.text-left").attr("href");
+      const title = $(this).find("a.text-left").text();
+      const imgSrc = $(this).find("img").attr("src");
+      const theme = "agriculture";
+      articles.push({
+        href,
+        title,
+        imgSrc,
+        theme,
+      });
     });
-  
-  }).catch(err => console.log(err))
-  const secondUrl = 'https://jamaity.org/associations/page/2';
-  axios(secondUrl).then(result => {
-        const html = result.data;
-        const $ = cheerio.load(html);
-  
-        // Scrape the information from the second page using Cheerio
-        $('div.panel').each(function () {
-            const href = $(this).find('a.text-left').attr('href');
-            const title = $(this).find('a.text-left').text();
-            const imgSrc = $(this).find('img').attr('src');
-            articles.push({
-                title,
-                href, 
-                imgSrc
-            });
-        });
-    })
-  
-  const ThirdUrl = 'https://jamaity.org/associations/page/3';
-  axios(ThirdUrl).then(result => {
-        const html = result.data;
-        const $ = cheerio.load(html);
-  
-        // Scrape the information from the second page using Cheerio
-        $('div.panel').each(function () {
-            const href = $(this).find('a.text-left').attr('href');
-            const title = $(this).find('a.text-left').text();
-            const imgSrc = $(this).find('img').attr('src');
-            articles.push({
-                title,
-                href, 
-                imgSrc
-            });
-        });
-    })
-  
-  
-    const FourthUrl = 'https://jamaity.org/associations/page/4';
-  axios(FourthUrl).then(result => {
-        const html = result.data;
-        const $ = cheerio.load(html);
-  
-        // Scrape the information from the second page using Cheerio
-        $('div.panel').each(function () {
-            const href = $(this).find('a.text-left').attr('href');
-            const title = $(this).find('a.text-left').text();
-            const imgSrc = $(this).find('img').attr('src');
-            articles.push({
-                title,
-                href, 
-                imgSrc
-            });
-        });
-    })
-  
-  
-  const FifthUrl = 'https://jamaity.org/associations/page/5';
-  axios(FifthUrl).then(result => {
-        const html = result.data;
-        const $ = cheerio.load(html);
-  
-        // Scrape the information from the second page using Cheerio
-        $('div.panel').each(function () {
-            const href = $(this).find('a.text-left').attr('href');
-            const title = $(this).find('a.text-left').text();
-            const imgSrc = $(this).find('img').attr('src');
-            articles.push({
-                title,
-                href, 
-                imgSrc
-            });
-        });
-    })
-    const SixthUrl = 'https://jamaity.org/associations/page/6';
-    axios(SixthUrl).then(result => {
-            const html = result.data;
-            const $ = cheerio.load(html);
-    
-            // Scrape the information from the second page using Cheerio
-            $('div.panel').each(function () {
-                const href = $(this).find('a.text-left').attr('href');
-                const title = $(this).find('a.text-left').text();
-                const imgSrc = $(this).find('img').attr('src');
-                articles.push({
-                    title,
-                    href, 
-                    imgSrc
-                });
-            });
-        })
-  
-        const Sevenurl = 'https://jamaity.org/associations/page/7';
-        axios(Sevenurl).then(result => {
-                const html = result.data;
-                const $ = cheerio.load(html);
-        
-                // Scrape the information from the second page using Cheerio
-                $('div.panel').each(function () {
-                    const href = $(this).find('a.text-left').attr('href');
-                    const title = $(this).find('a.text-left').text();
-                    const imgSrc = $(this).find('img').attr('src');
-                    articles.push({
-                        title,
-                        href, 
-                        imgSrc
-                    });
-                });
-            })
-    
-            const Eighturl = 'https://jamaity.org/associations/page/8';
-        axios(Eighturl).then(result => {
-                const html = result.data;
-                const $ = cheerio.load(html);
-        
-                // Scrape the information from the second page using Cheerio
-                $('div.panel').each(function () {
-                    const href = $(this).find('a.text-left').attr('href');
-                    const title = $(this).find('a.text-left').text();
-                    const imgSrc = $(this).find('img').attr('src');
-                    articles.push({
-                        title,
-                        href, 
-                        imgSrc
-                    });
-                });
-            })
-            const Nineurl = 'https://jamaity.org/associations/page/9';
-            axios(Nineurl).then(result => {
-                    const html = result.data;
-                    const $ = cheerio.load(html);
-            
-                    // Scrape the information from the second page using Cheerio
-                    $('div.panel').each(function () {
-                        const href = $(this).find('a.text-left').attr('href');
-                        const title = $(this).find('a.text-left').text();
-                        const imgSrc = $(this).find('img').attr('src');
-                        articles.push({
-                            title,
-                            href, 
-                            imgSrc
-                        });
-                    });
-                })
-  
-                const Tenurl = 'https://jamaity.org/associations/page/10';
-                axios(Tenurl).then(result => {
-                        const html = result.data;
-                        const $ = cheerio.load(html);
-                
-                        // Scrape the information from the second page using Cheerio
-                        $('div.panel').each(function () {
-                            const href = $(this).find('a.text-left').attr('href');
-                            const title = $(this).find('a.text-left').text();
-                            const imgSrc = $(this).find('img').attr('src');
-                            articles.push({
-                                title,
-                                href, 
-                                imgSrc
-                            });
-                        });
-                    })
+  })
+  .catch((err) => console.log(err));
+const secondUrl =
+  "https://jamaity.org/associations/?region=tunisie&theme=aide-handicapes";
+axios(secondUrl).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
 
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "aide-handicapes";
+    articles.push({
+      href,
+      title,
+      imgSrc,
+      theme,
+    });
+  });
+});
+
+const ThirdUrl =
+  "https://jamaity.org/associations/?region=tunisie&theme=aide-humanitaire";
+axios(ThirdUrl).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
+
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "aide-humanitaire";
+    articles.push({
+      href,
+      title,
+      imgSrc,
+      theme,
+    });
+  });
+});
+
+const FourthUrl =
+  "https://jamaity.org/associations/?region=tunisie&theme=artisanat";
+axios(FourthUrl).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
+
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "artisanat";
+    articles.push({
+      href,
+      title,
+      imgSrc,
+      theme,
+    });
+  });
+});
+
+const FifthUrl =
+  "https://jamaity.org/associations/?region=tunisie&theme=artisanat";
+axios(FifthUrl).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
+
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "artisanat";
+    articles.push({
+      href,
+      title,
+      imgSrc,
+      theme,
+    });
+  });
+});
+const SixthUrl =
+  "https://jamaity.org/associations/?region=tunisie&theme=arts-culture";
+axios(SixthUrl).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
+
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "arts-culture";
+    articles.push({
+      href,
+      title,
+      imgSrc,
+      theme,
+    });
+  });
+});
+
+const Sevenurl =
+  "https://jamaity.org/associations/?region=tunisie&theme=association-pro";
+axios(Sevenurl).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
+
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "association-pro";
+    articles.push({
+      href,
+      title,
+      imgSrc,
+      theme,
+    });
+  });
+});
+
+const Eighturl =
+  "https://jamaity.org/associations/?region=tunisie&theme=cinema";
+axios(Eighturl).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
+
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "cinema";
+    articles.push({
+      href,
+      title,
+      imgSrc,
+      theme,
+    });
+  });
+});
+const Nineurl =
+  "https://jamaity.org/associations/?region=tunisie&theme=citoyennete";
+axios(Nineurl).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
+
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "citoyennete";
+    articles.push({
+      title,
+      href,
+      imgSrc,
+      theme,
+    });
+  });
+});
+
+const Tenurl =
+  "https://jamaity.org/associations/?region=tunisie&theme=citoyennete-gouvernance";
+axios(Tenurl).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
+
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "citoyennete-gouvernance";
+    articles.push({
+      href,
+      title,
+      imgSrc,
+      theme,
+    });
+  });
+});
+
+const Tenurll =
+  "https://jamaity.org/associations/?region=tunisie&theme=droit-enfant";
+axios(Tenurll).then((result) => {
+  const html = result.data;
+  const $ = cheerio.load(html);
+
+  // Scrape the information from the second page using Cheerio
+  $("div.panel").each(function () {
+    const href = $(this).find("a.text-left").attr("href");
+    const title = $(this).find("a.text-left").text();
+    const imgSrc = $(this).find("img").attr("src");
+    const theme = "droit-enfant";
+    articles.push({
+      href,
+      title,
+      imgSrc,
+      theme,
+    });
+  });
+});
 module.exports = AssoRoutes;
